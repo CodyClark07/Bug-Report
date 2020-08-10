9a60ebcd3813b435ca3175168b9c6cc44ed2c43e
CodyClark07 <codyclark2010@hotmail.com>
2020-08-10 14:55:45 -0600

    built

--- clientsrc/src/components/Notes.vue
+++ clientsrc/src/components/Notes.vue
@@ -3,7 +3,7 @@
     <ul>
       <li>
         <h5 class="text-capitalize">
-          <span>{{this.$auth.userInfo.name}}: {{noteData.content}}</span>
+          <span>{{noteData.creatorEmail}}: {{noteData.content}}</span>
 
           <i
             v-if="this.$auth.userInfo"
